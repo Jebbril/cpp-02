e22172f1d0a2b70a1c7d960597a20d54622b9df9
Jebbril <orakib@student.1337.ma>
2023-08-01 20:11:53 +0100

    only last ex left

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -1,28 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
+/*   Point.cpp                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: orakib <orakib@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/30 17:26:41 by orakib            #+#    #+#             */
-/*   Updated: 2023/08/01 19:32:38 by orakib           ###   ########.fr       */
+/*   Created: 2023/08/01 19:56:12 by orakib            #+#    #+#             */
+/*   Updated: 2023/08/01 19:56:12 by orakib           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "Fixed.hpp"
-#include <cmath>
-
-int main( void ) {
-	Fixed a;
-	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
-	
-	std::cout << a << std::endl;
-	std::cout << ++a << std::endl;
-	std::cout << a << std::endl;
-	std::cout << a++ << std::endl;
-	std::cout << a << std::endl;
-	std::cout << b << std::endl;
-	std::cout << Fixed::max( a, b ) << std::endl;
-	return 0;
-}
